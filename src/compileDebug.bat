b60ba50b2c74c365f73c4ff4b5fa394106f1127c
Pimy <Vespimian@gmail.com>
2020-03-02 18:42:32 -0500

    Update and rename compileDebug.bat to src/compileDebug.bat

--- src/compileDebug.bat
+++ src/compileDebug.bat
@@ -1,13 +1,14 @@
 echo off
 cls
 
-xcopy /S /Y headers ..
+echo d | xcopy /S /Y headers ..\headers
 xcopy /Y main.c ..
 xcopy /Y initialize.c ..
 xcopy /Y data.c ..
 xcopy /Y drawFunctions.c ..
 
 cd ..
+
 
 echo.
 echo.
@@ -18,6 +19,12 @@ tcc -Werror -Wall -lSDL2 -o main.exe main.c data.c initialize.c drawFunctions.c
 echo.
 echo.
 echo.
+
+del main.c
+del initialize.c
+del data.c
+del drawFunctions.c
+rmdir /S /Q headers
 
 xcopy /Y main.exe SDL_Game\
 
