59735295d7c8e27b943dd4a347101bfdc0085b54
Pimy <Vespimian@gmail.com>
2020-03-02 18:55:18 -0500

    Update and rename headers/drawFunctions.h to src/headers/drawFunctions.h

--- headers/drawFunctions.h
+++
@@ -1,6 +0,0 @@
-#ifndef DRAWFUNCTIONS_H_
-#define DRAWFUNCTIONS_H_
-
-void DrawCircle(SDL_Renderer *renderer, int x, int y, float radius);
-
-#endif
---
+++ src/headers/drawFunctions.h
@@ -0,0 +1,6 @@
+#ifndef DRAWFUNCTIONS_H_
+#define DRAWFUNCTIONS_H_
+
+void DrawCircle(SDL_Renderer *renderer, int xc, int yc, int r);
+
+#endif
